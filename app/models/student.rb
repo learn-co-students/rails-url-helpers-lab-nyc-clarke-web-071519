3c89036b334da696e99a39ca4a97903bc931b06d
Valentino Wong <valentino.wong@gmail.com>
2019-08-08 17:12:25 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def change_status
+    if active == true
+      self.update(active: false)
+    else
+      self.update(active: true)
+    end
+  end
+
 end
